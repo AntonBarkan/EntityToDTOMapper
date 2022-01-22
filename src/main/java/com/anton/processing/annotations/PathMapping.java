bff231cfb2ce4d1a6167bf382e317429e358bee2
Anton <anton.barkan@dig.security>
2022-01-22 13:53:05 +0200

    init

--- src/main/java/com/anton/processing/annotations/PathMapping.java
+++ src/main/java/com/anton/processing/annotations/PathMapping.java
@@ -5,7 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
-public @interface BuilderProperty {
+public @interface PathMapping {
+    String value();
 }
